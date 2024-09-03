c229766daeec84dc9a7640decbfe8e8f1e8bd687
Pooja <poojasawant1030@gmail.com>
2024-09-03 15:25:24 +0530

    updated the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter} from 'react-router-dom';
+import ShopContextProvider, { ShopContext } from './context/ShopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+  <ShopContextProvider>
     <App />
-  </StrictMode>,
+  </ShopContextProvider>
+  </BrowserRouter>,
 )
