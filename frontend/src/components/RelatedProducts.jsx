d598536160f017547d688331b93c23a461f3c425
Pooja <poojasawant1030@gmail.com>
2024-09-17 23:55:49 +0530

    Updated the Pages

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -3,6 +3,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import Product from '../pages/Product';
+import ProductItem from './ProductItem';
 
 const RelatedProducts = ({category, subCategory}) => {
 
@@ -26,8 +28,10 @@ const RelatedProducts = ({category, subCategory}) => {
       <div className='text-center text-3xl py-2'>
         <Title text1={'RELATED'} text2={"PRODUCTS"} />
       </div>
-      <div className=''>
-
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {related.map((item, index)=>(
+            <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+        ))}
       </div>
     </div>
   )
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,49 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
+
+
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'AOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt='' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>iyphy woyhp wyhp</p>
+          <p>wyiphw wyhp</p>
+          <b>Our Mission</b>
+          <p>ioajptg pajhy</p>
+        </div>
+      </div>
+
+    <div className='text-4xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+    </div>
+
+    <div className='flex flex-col md:flex-row text-sm mb-20'>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance</b>
+          <p className='text-gray-600'>mgs ojga iaog</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>mgs ojga iaog</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>mgs ojga iaog</p>
+      </div>
+
+    </div>
+
+    <NewsletterBox />
     </div>
   )
 }
