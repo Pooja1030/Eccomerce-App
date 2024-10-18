975d25cb49a39fbbd1f8f6cf4e46a6a7a87beba6
Pooja <poojasawant1030@gmail.com>
2024-10-18 23:37:30 +0530

    fixed the imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
-import '../node_modules/react-toastify/dist/ReactToastify.css';
-
+import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify'
 
 const App = () => {
