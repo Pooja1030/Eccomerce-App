20c0080631acce4bcd4d8204b08e9ca3d5046a48
Pooja <poojasawant1030@gmail.com>
2024-10-18 17:27:38 +0530

    minor fixes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-import productModel from '../models/productmodel.js';
+import productModel from '../models/productModel.js';
 
 // Function for adding a product
 const addProduct = async (req, res) => {
