33d5c53ffa12c6e495fa8673ad535d811497b339
Pooja <poojasawant1030@gmail.com>
2024-10-18 18:48:32 +0530

    minor changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: [],
+      external: ['react-router-dom'], 
     },
   },
   resolve: {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: [],
+      external: ['react-router-dom'], 
     },
   },
   resolve: {
