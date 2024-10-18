94e7715035e02a30d77a85595e0666297f27119e
Pooja <poojasawant1030@gmail.com>
2024-10-18 18:28:03 +0530

    updated the code

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port : 5174},
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Example alias, modify according to your project structure
-    },
-  },
-
+  server : {port : 5174}
 })
-
-
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Example alias, modify according to your project structure
-    },
-  },
 })
