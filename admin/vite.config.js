b9d69dbc58f9ea24976c64bd20128d28bda33c96
Pooja <poojasawant1030@gmail.com>
2024-10-18 18:16:56 +0530

    fixed minor issue

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port : 5174}
+  server : {port : 5174},
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Example alias, modify according to your project structure
+    },
+  },
+
 })
+
+
